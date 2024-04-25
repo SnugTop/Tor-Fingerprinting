acbefafc5f2dffeff5349a5bde31b2682f11d7db
SnugTop <camjdaly@gmail.com>
2024-04-24 21:34:08 -0400

    Was not exporting correctly to the excel file. Add more parameters and made it so that I could keep adding to the same file.

--- data_analysis.py
+++ data_analysis.py
@@ -38,13 +38,17 @@ def main (pcap_file, gaurd_relay_ip):
     excel_file = 'pcap_analysis.xlsx'
 
     if os.path.exists(excel_file):
-        with pd.ExcelWriter(excel_file, mode='a') as writer:
-            df.to_excel(writer, sheet_name='pcap_data', index=False)
-            pd.Series(stats).to_frame('value').to_excel(writer, sheet_name='stats')
+        with pd.ExcelWriter(excel_file, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+            df.to_excel(writer, sheet_name='Packet Data', index=False, startrow=writer.sheets['Packet Data'].max_row, header=False)
+            pd.DataFrame([stats]).to_excel(writer, sheet_name='Statistics', index=False, startrow=writer.sheets['Statistics'].max_row, header=False)
     else:
-        with pd.ExcelWriter(excel_file) as writer:
-            df.to_excel(writer, sheet_name='pcap_data', index=False)
-            pd.Series(stats).to_frame('value').to_excel(writer, sheet_name='stats')
+        with pd.ExcelWriter(excel_file, engine='openpyxl') as writer:
+            df.to_excel(writer, sheet_name='Packet Data', index=False)
+            pd.DataFrame([stats]).to_excel(writer, sheet_name='Statistics', index=False)
+
+    # Print stats to console
+    print(f"Statistics Saved to Excel: {excel_file}")
+    print(pd.DataFrame([stats]))
 
     print(df)
     print(stats)
@@ -56,3 +60,7 @@ if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('Usage: python data_analysis.py <pcap_file> <gaurd_relay_ip>')
         sys.exit(1)
+
+    pcap_file = sys.argv[1]
+    guard_relay_ip = sys.argv[2]
+    main(pcap_file, guard_relay_ip)
